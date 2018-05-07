98d251eb64739d61e8498b4d8ee0b3e2769b078a
DashaDaria <dashadariallc@gmail.com>
2018-05-07 16:24:25 -0600

    added a css framework 'semantic ui'
    '
    end
    exit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Dasha Daria</h1>
         </header>
         <p className="App-intro">
           About
