6728124c0802c9dfa08791961a53919794881129
DashaDaria <dashadariallc@gmail.com>
2018-05-09 17:45:54 -0600

    added particles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import GridSystem from './GridSystem';
+import ddlogo from './ddlogo.png'
+import Particles from 'react-particles-js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Dasha Daria</h1>
-        </header>
-        <p className="App-intro">
-          About
-        </p>
+          <img src={ddlogo} className="logo" alt="logo" />
+        <div className="App-body">
         <GridSystem />
+        </div>
+
+        <Particles
+          className="particles"
+          params={{
+            particles: {
+              number: {value: 30}
+            }
+          }}
+        />
       </div>
     );
   }
--- src/GridSystem.js
+++ src/GridSystem.js
@@ -1,29 +1,14 @@
 import React from 'react'
-import { Grid, Image } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
 
 const GridSystem = () => (
   <Grid divided='vertically'>
-    <Grid.Row columns={2}>
+    <Grid.Row columns={1}>
       <Grid.Column>
-        column 1
-      </Grid.Column>
-      <Grid.Column>
-        column 2
-      </Grid.Column>
-    </Grid.Row>
-
-    <Grid.Row columns={3}>
-      <Grid.Column>
-      column 1
-      </Grid.Column>
-      <Grid.Column>
-        column 2
-      </Grid.Column>
-      <Grid.Column>
-        column 3
+        <p className="intro">Dasha <br/> Daria</p>
       </Grid.Column>
     </Grid.Row>
   </Grid>
 )
 
-export default GridSystem 
+export default GridSystem
