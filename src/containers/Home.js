f5bba19686a93ac6f6133848aabba9c33f1b8437
DashaDaria <dashadariallc@gmail.com>
2018-05-22 16:09:21 -0600

    added trait animation

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -6,39 +6,53 @@ import './Home.css';
 import ProjectCard from '../components/ProjectCard';
 import patrolli from './patrolli.jpg';
 import elevator from './elevator.jpg';
+import pitch from './pitch.jpg';
+import workout from './workout.jpg';
+
+const traits =
+  ["Full Stack Problem Solver", "Interactive Platform Developer",
+    "Integrated Content Creator", "Web Designer", "Passionate Learner"]
 
 
 class Home extends Component {
+  constructor() {
+    super()
+    this.state = {traitIdx: 0 };
+    }
 
+    componentDidMount(){
+      this.traitId = setInterval(() => {
+        let currentIdx = this.state.traitIdx
+        this.setState({traitIdx: ++currentIdx })
+      }, 2000)
+    }
+
+    componentWillUnmount(){
+      clearInterval(this.traitId);
+    }
 
   render() {
+    let traitChanger = traits[this.state.traitIdx % traits.length]
     return (
       <div className="App">
           <img src={ddlogo} className="main-logo" alt="logo" />
         <div className="intro-container">
-            <p className="intro">DASHA<br/><span className="daria">daria</span></p>
+            <p className="intro">DASHA DARIA</p>
             <div className="left-bracket">[</div>
-            <div className="quote">Full Stack Problem Solver</div>
-            <div className="right-bracket">]</div>
+            <div className="quote">{traitChanger}</div>
+            <div className="right-bracket">]</div><br/>
             <div><button>Contact Me</button></div>
           </div>
 
-            <div className="web-projects">
-            <h3 className="project-category">interactive <br/>web <br/>developer
-              <img src={ddlogo} className="logo-tag" alt="logo" />
-            </h3>
-              <ProjectCard project="Virtual Elevator" image={elevator}/>
-              <ProjectCard project="Pitch Or Ditch"/>
-              <ProjectCard project="Workout Buddy"/>
-            </div>
 
-            <div className="marketing-projects">
-            <h3 className="project-category">integrated <br/>content <br/>creator
-              <img src={ddlogo} className="logo-tag" alt="logo" />
-            </h3>
+            <div className="projects">
+              <div className="projects-header">PROJECTS</div>
+              <ProjectCard project="Virtual Elevator" image={elevator}/>
+              <ProjectCard project="Pitch Or Ditch" image={pitch}/>
+              <ProjectCard project="Workout Buddy" image={workout}/>
               <ProjectCard project="Flonase Sensimist and Focused" image={patrolli}/>
-              <ProjectCard project="Trolli and SDCC"/>
-              <ProjectCard project="Dodge and Sharknado"/>
+              <ProjectCard project="Trolli and SDCC" image={patrolli}/>
+              <ProjectCard project="Dodge and Sharknado" image={patrolli}/>
             </div>
 
 
